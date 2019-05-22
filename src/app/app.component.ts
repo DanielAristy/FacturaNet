6b0219d79c87c1646a337387672a260471252d92
DanielAristy <danielariaty@gmail.com>
2019-05-22 13:48:03 -0500

    siguiente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title:string = 'Angular Crud';
+
+  empleados = [
+    {'nombre': 'Daniel', posicion: 'Profesor', email:'dan.y.ac@hotmail.com'},
+    {'nombre': 'Juan', posicion: 'Programador', email:'judac25@hotmail.com'},
+    {'nombre': 'Pedro', posicion: 'Diseñador', email:'pedro22@hotmail.com'}
+  ];
+
+  model:any = {}
+  //Agragar empleado
+  addEmpleado(): void{
+    this.empleados.push(this.model);
+  }
+
+  //Eliminar empleado
+  deleteEmpleado():void{
+
+  }
+
+  //Editar empleado
+  editEmpleado():void{
+
+  }
+
+  //Actualizar empleado
+  updateEmpleado():void{
+   
+  }
 }
