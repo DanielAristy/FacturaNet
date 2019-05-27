3ed97c931530e921cd2f8514f593b029dc9232a6
DanielAristy <danielariaty@gmail.com>
2019-05-27 02:47:23 -0500

    Arreglando fallos de las versiones en el caso de AngularCore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { modelGroupProvider } from '@angular/forms/src/directives/ng_model_group';
 
 @Component({
